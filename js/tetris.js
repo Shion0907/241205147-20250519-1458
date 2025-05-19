f5e6e77bfb8168d8561fd29108aab180b41e0fd7
Shion Bandai <241205147@ccmailg.meijo-u.ac.jp>
2025-05-19 15:13:48 +0900

    Update tetris.js

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'green', 'green', 'green', 'green', 'green', 'green', 'green'
+    'skyblue', 'orange', 'blue', 'yellow', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
